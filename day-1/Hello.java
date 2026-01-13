745048554e0f1dea60f477b0e1536d8948937e5c
Sayantan Nandi <sayantannandi13@gmail.com>
2026-01-13 12:51:06 +0530

    day 1

---
+++ day-1/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
+
+// Homework Program :
+// 1. Write a program to add two numbers
+// 2. WAP to multiply 2 numbers
+// 3. WAP to print my name
+// 4. WAP to print my best friend's name
